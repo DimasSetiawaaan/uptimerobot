762bd4800c711a224b6e354f26085f0c4e9ff748
DimasSetiawaaan <79965774+DimasSetiawaaan@users.noreply.github.com>
2021-05-04 12:08:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://enzim-bot.glitch.me"]
+const urls = ["https://kenzy-bots.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
