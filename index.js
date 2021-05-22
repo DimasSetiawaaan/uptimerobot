9bbe44a6beb688116b1473091e4bfa18b439e984
DimasSetiawaaan <79965774+DimasSetiawaaan@users.noreply.github.com>
2021-05-23 02:20:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://kiritos-bot.glitch.me"]
+const urls = ["https://kiritos-backup.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
