76eca298fdc115a48f167c1e34e57769da5f5b0d
DimasSetiawaaan <79965774+DimasSetiawaaan@users.noreply.github.com>
2021-06-13 15:53:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://1-mounth-tl.glitch.me"]
+const urls = ["https://lambang-bot.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
