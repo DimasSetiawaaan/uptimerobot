feab57abc4eb384bb031ef9e01abfcd86b845d15
DimasSetiawaaan <79965774+DimasSetiawaaan@users.noreply.github.com>
2021-06-25 01:30:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://xm-radio.glitch.me"]
+const urls = ["https://Discord-MusicBot-1.kenjinicholas.repl.co"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
